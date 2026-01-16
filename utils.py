7022fb0f287b9d961a09afa17bbc960ce9710450
mobifonernd2025-collab <mobifonernd2025@gmail.com>
2026-01-16 08:16:28 +0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,56 @@
+# utils.py
+import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import io
+import base64
+from gtts import gTTS
+import speech_recognition as sr
+from config import FILE_ID, COL_ENG, COL_VIE # <--- Đã sửa dòng này
+
+# Hàm tạo audio
+def get_audio_base64(text):
+    if not text: return None
+    try:
+        tts = gTTS(text=text, lang='en')
+        fp = io.BytesIO()
+        tts.write_to_fp(fp)
+        fp.seek(0)
+        b64 = base64.b64encode(fp.read()).decode()
+        return f"data:audio/mp3;base64,{b64}"
+    except: return None
+
+# Hàm nhận diện giọng nói
+def recognize_speech(audio_bytes):
+    r = sr.Recognizer()
+    try:
+        audio_file = io.BytesIO(audio_bytes)
+        with sr.AudioFile(audio_file) as source:
+            audio_data = r.record(source)
+            text = r.recognize_google(audio_data, language="en-US")
+            return text.lower()
+    except: return "kém_chất_lượng"
+
+# Hàm kết nối Google Sheet
+@st.cache_resource(ttl=60)
+def get_gspread_client():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    try:
+        key_dict = dict(st.secrets["gcp_service_account"])
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(key_dict, scope)
+        return gspread.authorize(creds)
+    except Exception as e:
+        st.error(f"Lỗi kết nối: {e}")
+        return None
+
+# Hàm tải dữ liệu
+def load_data():
+    try:
+        client = get_gspread_client()
+        if not client: return []
+        spreadsheet = client.open_by_key(FILE_ID)
+        sheet_name = st.session_state.get('selected_sheet_name')
+        if sheet_name: ws = spreadsheet.worksheet(sheet_name)
+        else: ws = spreadsheet.get_worksheet(0)
+        return [r for r in ws.get_all_records() if r.get(COL_ENG) and r.get(COL_VIE)]
+    except: return []
